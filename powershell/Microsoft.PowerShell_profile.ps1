9fe30be8c002aff7952002677393c80495eefdc8
Manfred Wallner <schusterfredl@mwallner.net>
2018-12-12 19:40:39 +0100

    colored l (ls) in PowerShell

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -47,7 +47,66 @@ function Start-ElevatedProcess {
   [System.Diagnostics.Process]::Start($psi);
 }
 
+
+# kudos https://gist.github.com/haf/1313318
+function Get-ChildItemColor {
+  <#
+  .Synopsis
+    Returns childitems with colors by type.
+    From http://poshcode.org/?show=878
+  .Description
+    This function wraps Get-ChildItem and tries to output the results
+    color-coded by type
+  .ReturnValue
+    All objects returned by Get-ChildItem are passed down the pipeline
+    unmodified.
+  .Notes
+    NAME:      Get-ChildItemColor
+    AUTHOR:    Tojo2000 <tojo2000@tojo2000.com>
+  #>
+  $regex_opts = ([System.Text.RegularExpressions.RegexOptions]::IgnoreCase `
+      -bor [System.Text.RegularExpressions.RegexOptions]::Compiled)
+  
+  $fore = $Host.UI.RawUI.ForegroundColor
+  $compressed = New-Object System.Text.RegularExpressions.Regex(
+    '\.(zip|tar|gz|rar|7z)$', $regex_opts)
+  $executable = New-Object System.Text.RegularExpressions.Regex(
+    '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg|fsx|sh)$', $regex_opts)
+  $dll_pdb = New-Object System.Text.RegularExpressions.Regex(
+    '\.(dll|pdb)$', $regex_opts)
+  $configs = New-Object System.Text.RegularExpressions.Regex(
+    '\.(config|conf|ini)$', $regex_opts)
+  $text_files = New-Object System.Text.RegularExpressions.Regex(
+    '\.(txt|cfg|conf|ini|csv|log)$', $regex_opts)
+  
+  Invoke-Expression ("Get-ChildItem $args") |
+    ForEach-Object {
+    $c = $fore
+    if ($_.GetType().Name -eq 'DirectoryInfo') {
+      $c = 'DarkCyan'
+    }
+    elseif ($compressed.IsMatch($_.Name)) {
+      $c = 'Yellow'
+    }
+    elseif ($executable.IsMatch($_.Name)) {
+      $c = 'Green'
+    }
+    elseif ($text_files.IsMatch($_.Name)) {
+      $c = 'Cyan'
+    }
+    elseif ($dll_pdb.IsMatch($_.Name)) {
+      $c = 'DarkGreen'
+    }
+    elseif ($configs.IsMatch($_.Name)) {
+      $c = 'Yellow'
+    }
+    $Host.UI.RawUI.ForegroundColor = $c
+    Write-Output $_
+    $Host.UI.RawUI.ForegroundColor = $fore
+  }
+}
+
 ### various aliases ###
 Set-Alias -Name sudo -Value Start-ElevatedProcess
 Set-Alias -Name c -Value code  
-
+Set-Alias -Name l -Value Get-ChildItemColor
