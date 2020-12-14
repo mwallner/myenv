9de987c9968adcf2d75507feda9018e90ac1d2f4
mw <Manfred Wallner>
2020-12-14 19:22:06 +0100

    vim setup instructions

---
+++ vim_setup.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+
+git clone https://github.com/drmingdrmer/vim-tabbar.git ~/.vim/pack/vendor/start/vim-tabbar
+
+# vim-youcompleteme
+# vam install youcompleteme
+# vam install powerline
+
